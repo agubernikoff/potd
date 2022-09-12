f3fde4bbf0353fba43db9e4b14a560c9767a5b07
alexander gubernikoff <agubernikoff@gmail.com>
2022-09-12 02:37:47 -0400

    save users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     
     def index
       users = User.all
-      users.save_all
+      User.find_each(&:save)
       render json: users
     end
     
