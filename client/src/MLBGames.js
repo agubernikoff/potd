01a4b5d6fbbb23ddd0fc119ba4561452b053fa3e
alexander gubernikoff <agubernikoff@gmail.com>
2022-10-28 11:13:40 -0400

    world series

--- client/src/MLBGames.js
+++ client/src/MLBGames.js
@@ -1,67 +1,36 @@
 function MLBGames() {
   const teams = {
-    padres: {
-      team: "San Diego Padres",
-      abbreviation: "SD",
-      starter: "Y. Darvish",
-    },
     phillies: {
       team: "Philladelphia Phillies",
       abbreviation: "PHI",
-      starter: "Z. Wheeler",
-    },
-    yankees: {
-      team: "New York Yankees",
-      abbreviation: "NYY",
-      starter: "N. Cortes",
+      starter: "A. Nola",
     },
     astros: {
       team: "Houston Astros",
       abbreviation: "HOU",
-      starter: "L. McCullers",
+      starter: "J. Verlander",
     },
   };
   const games = [
     {
-      schedule: { start: "2022-10-23T18:38:00.000Z" },
+      schedule: { start: "2022-10-29T00:04:00.000Z" },
       teams: {
-        away: teams.padres,
-        home: teams.phillies,
+        away: teams.phillies,
+        home: teams.astros,
       },
       odds: {
         spread: {
           current: {
             away: "+1.5",
             home: "-1.5",
-            awayOdds: "-185",
-            homeOdds: "+155",
+            awayOdds: "-165",
+            homeOdds: "+135",
           },
         },
       },
-      moneyline: { current: { awayOdds: "+120", homeOdds: "-140" } },
+      moneyline: { current: { awayOdds: "+140", homeOdds: "-165" } },
       total: {
-        current: { total: "6.5", overOdds: "-115", underOdds: "-105" },
-      },
-    },
-    {
-      schedule: { start: "2022-10-23T23:08:00.000Z" },
-      teams: {
-        away: teams.astros,
-        home: teams.yankees,
-      },
-      odds: {
-        spread: {
-          current: {
-            away: "-1.5",
-            home: "+1.5",
-            awayOdds: "+170",
-            homeOdds: "-210",
-          },
-        },
-      },
-      moneyline: { current: { awayOdds: "+105", homeOdds: "-120" } },
-      total: {
-        current: { total: "6.5", overOdds: "-120", underOdds: "+100" },
+        current: { total: "6.5", overOdds: "-130", underOdds: "+105" },
       },
     },
   ];
