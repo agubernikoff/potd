1632c67528b4c273e26581c1f2aa86a3e9581e7c
alexander gubernikoff <agubernikoff@gmail.com>
2022-11-03 11:21:56 -0400

    mlb

--- client/src/MLBGames.js
+++ client/src/MLBGames.js
@@ -3,18 +3,18 @@ function MLBGames() {
     phillies: {
       team: "Philadelphia Phillies",
       abbreviation: "PHI",
-      starter: "A. Nola",
+      starter: "N. Syndergaard",
     },
     astros: {
       team: "Houston Astros",
       abbreviation: "HOU",
-      starter: "C. Javier",
+      starter: "J. Verlander",
     },
   };
   const games = [
     {
       details: { league: "MLB" },
-      schedule: { start: "2022-11-03T00:04:00.000Z" },
+      schedule: { start: "2022-11-04T00:04:00.000Z" },
       teams: {
         away: teams.astros,
         home: teams.phillies,
@@ -24,14 +24,14 @@ function MLBGames() {
           current: {
             away: "-1.5",
             home: "+1.5",
-            awayOdds: "+160",
-            homeOdds: "-190",
+            awayOdds: "+110",
+            homeOdds: "-130",
           },
         },
       },
-      moneyline: { current: { awayOdds: "-105", homeOdds: "-115" } },
+      moneyline: { current: { awayOdds: "-155", homeOdds: "-130" } },
       total: {
-        current: { total: "7.5", overOdds: "+105", underOdds: "-125" },
+        current: { total: "7.5", overOdds: "-105", underOdds: "-115" },
       },
     },
   ];
