23abae86da4744f2e58d4277be2e04a739acbdfa
alexander gubernikoff <agubernikoff@gmail.com>
2022-11-19 09:27:07 -0500

    removed login process for reid to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   skip_before_action :is_logged_in?, only: :create
     def create
       user = User.find_by(username: params[:username])
-      if user&.authenticate(params[:password])
+      # if user&.authenticate(params[:password])
         session[:user_id] = user.id
         render json: user, status: :created
-      else
-        render json: { errors: ["Incorrect username and password"] }, status: :unauthorized
-      end
+      # else
+      #   render json: { errors: ["Incorrect username and password"] }, status: :unauthorized
+      # end
     end
   
     def destroy
